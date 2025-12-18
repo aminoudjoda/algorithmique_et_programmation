e4fbfdcba3e4732a48ba72a00027525a3186d6eb
aminoudjoda <aminoudjodda1@gmail.com>
2025-12-18 15:00:24 +0100

    ajout du fichier main.c

---
+++ main.c
@@ -0,0 +1,15 @@
+
+int main( void ) {
+int result = power( 2, 3 );
+printf( "2³ == %d\n", result );
+result = fact( 6 );
+printf( "6! == %d\n", result );
+
+int first = 10;
+int second =16;
+printf("first=%d , second=%d\n", first, second);
+permutation(&first,&second);
+printf("first=%d , second=%d", first, second);
+
+return EXIT_SUCCESS;
+}
